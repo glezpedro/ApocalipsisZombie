e7a74bc089e199477055a8eb706ec27fc7cd806b
Angel Molinero <angelmolinerocaja@gmail.com>
2025-01-26 16:28:33 +0100

    Delete ZOMBIEFINAL directory

--- ZOMBIEFINAL/src/Normal.java
+++
@@ -1,9 +0,0 @@
-
-import java.io.Serializable;
-
-
-public class Normal extends Zombi implements Serializable{
-    public Normal(TipoZombie tipo, int x, int y) {
-        super(tipo,x ,y, "NORMAL");
-    }
-}
